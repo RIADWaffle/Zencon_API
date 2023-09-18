22b970f399e2ab9f7613202f9d2442673eb13910
RIADWaffle <106703366+RIADWaffle@users.noreply.github.com>
2023-09-18 11:04:43 -0600

    API - CSV
    Both added to the repository

---
+++ API.py
@@ -0,0 +1,35 @@
+# Import the necessary libraries
+import plotly.graph_objects as go
+import pandas as pd
+from flask import Flask, request, jsonify
+
+#Read the dataset and get the correlation
+data = pd.read_csv('datos3.csv')
+corr = data.corr()
+
+# Create a Flask app
+app = Flask(__name__)
+
+
+# Define the route for the API
+@app.route('/corelation', methods=['GET'])
+
+
+def api():
+    # Create a heatmap
+    fig = go.Figure(data=go.Heatmap(
+        x=corr.columns,
+        y=corr.columns,
+        z=corr,
+        colorscale="Tealgrn",
+        transpose= True
+    ))
+
+    # Return the response
+    return fig.to_json()
+
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run()
